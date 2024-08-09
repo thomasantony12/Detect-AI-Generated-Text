6c6ef2044379e70c8bac66c88bb9e987980416ae
Thomas antony <thomasantony12042001@gmail.com>
2024-08-09 11:07:51 +0530

    project completed
    some changes are still there

---
+++ Aidetecter.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import joblib
+
+# Load the trained model and vectorizer
+model = joblib.load('./Model/Jupyter files/ai_text_detector.pkl')
+vectorizer = joblib.load('./Model/Jupyter files/vectorizer.pkl')
+
+def predict_text_type(text):
+    # Transform the text using the vectorizer
+    text_vectorized = vectorizer.transform([text])
+    
+    # Predict using the loaded model
+    prediction = model.predict(text_vectorized)
+    
+    # Interpret the result
+    result = 'AI-generated' if prediction[0] == 1 else 'Human-written'
+    return result
+
+st.title("Ai Text Detection")
+st.subheader("Enter your desired text", divider=True)
+in_text = st.text_area(label="")
+
+if st.button("Recognize"):
+    out_text = predict_text_type(in_text)
+    st.divider()
+    st.header(out_text)
